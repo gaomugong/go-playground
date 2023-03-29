092e59c7551cfeeeb45a946346d022e6df3bdb88
gaomugong <gaomugong@qq.com>
2023-03-29 17:52:07 +0800

    hello map

--- tutorial/map.go
+++ tutorial/map.go
@@ -11,4 +11,26 @@ func main() {
 	m["k2"] = 13
 	fmt.Println("map: ", m)
 
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
+
+	fmt.Println("len: ", len(m))
+	delete(m, "k2")
+	fmt.Println("map: ", m)
+
+	// The optional second return value when getting a
+	// value from a map indicates if the key was present
+	// in the map. This can be used to disambiguate
+	// between missing keys and keys with zero values
+	// like `0` or `""`
+	k2, ok := m["k2"]
+	// k2, ok := m["k1"]
+	fmt.Printf("k2: %d, ok: %v\n", k2, ok)
+
+	// declare and initialize a new map in the same line.
+	n := map[string]int{
+		"foo": 1,
+		"bar": 2,
+	}
+	fmt.Println("map: ", n)
 }
