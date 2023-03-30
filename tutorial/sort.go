491a94b04f1b0d46685f25d4d7c94c9517a50c98
gaomugong <gaomugong@qq.com>
2023-03-30 18:50:55 +0800

    hello sort

--- tutorial/sort.go
+++ tutorial/sort.go
@@ -5,6 +5,22 @@ import (
 	"sort"
 )
 
+type byLength []string
+
+// Len 实现sort.Interface接口
+func (s byLength) Len() int {
+	return len(s)
+}
+
+// Less 按字符串长度排序
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
 func main() {
 	ints := []int{3, 2, 4, 5, 1, 8}
 	strs := []string{"c", "f", "b", "a"}
@@ -14,4 +30,8 @@ func main() {
 
 	sort.Ints(ints)
 	fmt.Println("ints: ", ints, sort.IntsAreSorted(ints))
+
+	animals := []string{"bear", "dog", "rabbit", "panda", "cat"}
+	sort.Sort(byLength(animals))
+	fmt.Println(animals)
 }
