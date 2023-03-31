300f18d0175a258e127f5041addeb89fae22db8d
gaomugong <gaomugong@qq.com>
2023-03-31 14:59:20 +0800

    hello struct

--- tutorial/struct.go
+++ tutorial/struct.go
@@ -14,10 +14,20 @@ func (r Rect) area() int {
 	return r.width * r.height
 }
 
+func (r *Rect) area1() int {
+	return r.width * r.height
+}
+
 func main() {
 	rect1 := Rect{3, 4}
 	rect2 := NewRect(3, 4)
+	rect3 := &rect1
 
 	fmt.Println("area:", rect1.area())
 	fmt.Println("area:", rect2.area())
+
+	// Go automatically handles conversion between values
+	// and pointers for method calls.
+	fmt.Println("area:", rect1.area1())
+	fmt.Println("area:", rect3.area1())
 }
