3bac0d3639d460f1351fb79ca69a7a14eeb58bff
gaomugong <gaomugong@qq.com>
2023-04-01 20:35:46 +0800

    hello wg and atomic

--- tutorial/recursion.go
+++ tutorial/recursion.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// factorial is a recursive function that computes the factorial of an integer.
+// The factorial of a given number n is defined as the product of all its numbers from 1 to n.
+// If n is equal to 0, the factorial is equal to 1.
 func factorial(n int) int {
 	if n == 0 {
 		return 1
