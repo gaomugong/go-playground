7d63aaa2270ef531d07f2a3522e258d34ffde9b4
gaomugong <gaomugong@qq.com>
2023-04-01 18:27:17 +0800

    hello channel

--- tutorial/channel.go
+++ tutorial/channel.go
@@ -12,4 +12,12 @@ func main() {
 	// read "ping" from messages
 	msg := <-messages
 	fmt.Println(msg)
+
+	// make a string channel buffering up to 2 values
+	messages1 := make(chan string, 2)
+	messages1 <- "buffered"
+	messages1 <- "channel"
+
+	fmt.Println(<-messages1)
+	fmt.Println(<-messages1)
 }
