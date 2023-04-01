c307402c4a2aef87f48d0565ed701df3b03aa5eb
gaomugong <gaomugong@qq.com>
2023-04-01 18:48:00 +0800

    hello select timeout

---
+++ tutorial/select_timeout.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// For our example, suppose we're executing an external
+	// call that returns its result on a channel `c1`
+	// after 2s. Note that the channel is buffered, so the
+	// send in the goroutine is nonblocking. This is a
+	// common pattern to prevent goroutine leaks in case the
+	// channel is never read.
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
+
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout 1")
+	}
+}
