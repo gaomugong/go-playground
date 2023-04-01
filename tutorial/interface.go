7baea1f4cb508df42ba17a1dd1f1086ff19bfb5c
gaomugong <gaomugong@qq.com>
2023-04-01 08:40:50 +0800

    hello interface

--- tutorial/interface.go
+++ tutorial/interface.go
@@ -44,10 +44,42 @@ func measure(g geometry) {
 	fmt.Println(g.perim())
 }
 
+// 定义一个可以接收任何参数的函数
+func myfunc(iface interface{}) {
+	fmt.Printf("type: %T, value: %v\n", iface, iface)
+}
+
 func main() {
 	r := rect{width: 3, height: 4}
 	c := circle{radius: 5}
 
 	measure(r)
 	measure(c)
+
+	// 空接口变量默认值为nil
+	var i interface{}
+	fmt.Printf("type: %T, value: %v\n", i, i)
+
+	// 空接口可以承载任意类型的值
+	i = 1
+	myfunc(i)
+	i = false
+	myfunc(i)
+	i = "hello"
+	myfunc(i)
+
+	// 类型断言
+	i1 := i.(string)
+	fmt.Println("i1: ", i1)
+	if i2, ok := i.(string); ok {
+		fmt.Printf("i is a string var, %s\n", i2)
+	}
+
+	// 定义一个可以接收任何类型的array/slice/map/struct
+	any1 := make([]interface{}, 5)
+	any1[0] = 1
+	any1[1] = "hello"
+	any1[2] = []int{1, 2, 3, 4, 5}
+	myfunc(any1)
+
 }
