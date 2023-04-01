bd5ea54df5d628b617b95d50b519aef6752d2c5b
gaomugong <gaomugong@qq.com>
2023-04-01 18:35:23 +0800

    hello select

---
+++ tutorial/select.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// select across two channels
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	// each channel receive a value after some seconds
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "second"
+	}()
+
+	// use select to await both of these values simultaneously and print
+	for i := 0; i < 2; i++ {
+		fmt.Println("use select to await both of these values")
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+
+}
