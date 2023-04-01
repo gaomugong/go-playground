e1afa92662cd3a65c3d4ebccd439cc4422d064ae
gaomugong <gaomugong@qq.com>
2023-04-01 18:20:48 +0800

    hello panic/recover

---
+++ tutorial/goroutine.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	go f("first goroutine")
+	go f("second goroutine")
+	// start a goroutine for an anonymous function call
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second * 8)
+	fmt.Println("done")
+}
