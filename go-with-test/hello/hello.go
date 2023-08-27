fd2bb7374b7906906e9c7f3df58ec5f1421e3c2f
gaomugong <gaomugong@qq.com>
2023-08-27 16:08:55 +0800

    hello v3

--- go-with-test/hello/hello.go
+++ go-with-test/hello/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishPrefix = "Hello "
+
 func Hello(name string) string {
-	return "Hello " + name
+	if name == "" {
+		name = "world"
+	}
+	return englishPrefix + name
 }
 func main() {
 	fmt.Println(Hello(""))
--- go-with-test/hello/hello_test.go
+++ go-with-test/hello/hello_test.go
@@ -11,11 +11,31 @@ import "testing"
 //	}
 //}
 
-func TestHello(t *testing.T) {
-	got := Hello("pitou")
-	want := "Hello pitou"
+//func TestHello(t *testing.T) {
+//	got := Hello("pitou")
+//	want := englishPrefix + "pitou"
+//
+//	if got != want {
+//		t.Errorf("got %q, want %q", got, want)
+//	}
+//}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+func TestHello(t *testing.T) {
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("pitou")
+		want := englishPrefix + "pitou"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("say hello world when name is empty", func(t *testing.T) {
+		got := Hello("")
+		want := englishPrefix + "world"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
