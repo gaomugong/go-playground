e6908c692445ea3cf67c3f2ed02cbe73e2ec11fe
gaohongsong <ghs@qq.com>
2023-08-29 11:29:15 +0800

    feat: 支持指定重复次数

--- go-with-test/iteration/repeat.go
+++ go-with-test/iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-func Repeat(s string) string {
+func Repeat(s string, num int) string {
 
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < num; i++ {
 		repeated += s
 	}
 	return repeated
