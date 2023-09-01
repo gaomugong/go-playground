187fba5a182f9b9fcabae1920b39da35db7d5f56
gaomugong <gaomugong@qq.com>
2023-09-01 11:51:18 +0800

    wallet.assert

--- go-with-test/wallets/wallet_test.go
+++ go-with-test/wallets/wallet_test.go
@@ -6,30 +6,25 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-		fmt.Println("wallet.balance address in test", &wallet.balance)
-
-		wallet.Deposit(10)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
 		got := wallet.Balance()
-		want := Bitcoin(10)
 
 		if got != want {
 			// Bitcoin implement Stringer interface
 			t.Errorf("got %s, want %s", got, want)
 		}
+	}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		fmt.Println("wallet.balance address in test", &wallet.balance)
+		wallet.Deposit(10)
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(10)
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
