44077ad6fbc350b000e71afc16a92491f68d9e72
gaomugong <gaomugong@qq.com>
2023-09-02 07:27:50 +0800

    errcheck

--- go-with-test/wallets/errwrap_test.go
+++ go-with-test/wallets/errwrap_test.go
@@ -15,6 +15,7 @@ func TestErrorWrap(t *testing.T) {
 		// 还原被包装后的错误，得到错误的"根源"
 		originalError := errors.Unwrap(err)
 		fmt.Println(originalError)
+		fmt.Println(reflect.TypeOf(err))
 		fmt.Println(reflect.TypeOf(originalError))
 		t.Errorf("got error: %v", err)
 	}
@@ -24,6 +25,6 @@ func ReadConfig() ([]byte, error) {
 	home := os.Getenv("HOME")
 	config, err := os.ReadFile(filepath.Join(home, ".settings.xml"))
 	//https://rollbar.com/blog/golang-wrap-and-unwrap-error/
-	// 利用%w包装错误
+	// 利用%w包装错误 *fs.PathError -> *fmt.wrapError
 	return config, fmt.Errorf("read config failed: %w", err)
 }
