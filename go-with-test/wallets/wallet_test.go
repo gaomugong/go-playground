57a3f82f8d6aeb5c3614f0b91b8349131fdd8f8e
gaomugong <gaomugong@qq.com>
2023-09-02 06:47:51 +0800

    errcheck

--- go-with-test/wallets/wallet_test.go
+++ go-with-test/wallets/wallet_test.go
@@ -5,15 +5,43 @@ import (
 	"testing"
 )
 
-func TestWallet(t *testing.T) {
-	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-		got := wallet.Balance()
+//指针
+//当你传值给函数或方法时，Go 会复制这些值。因此，如果你写的函数需要更改状态，你就需要用指针指向你想要更改的值
+//Go 取值的副本在大多数时候是有效的，但是有时候你不希望你的系统只使用副本，在这种情况下你需要传递一个引用
 
-		if got != want {
-			// Bitcoin implement Stringer interface
-			t.Errorf("got %s, want %s", got, want)
-		}
+//https://dave.cheney.net/2016/04/27/dont-just-check-errors-handle-them-gracefully
+//nil
+//指针可以是 nil
+//当函数返回一个的指针，你需要确保检查过它是否为 nil，否则你可能会抛出一个执行异常，编译器在这里不能帮到你
+//nil 非常适合描述一个可能丢失的值
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	got := wallet.Balance()
+
+	if got != want {
+		// Bitcoin implement Stringer interface
+		t.Errorf("got %s, want %s", got, want)
 	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("got an error but didn't want one")
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	if got == nil {
+		t.Errorf("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("get %s, want %s", got, want)
+	}
+}
+
+func TestWallet(t *testing.T) {
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		fmt.Println("wallet.balance address in test", &wallet.balance)
@@ -23,19 +51,10 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(10)
+		err := wallet.Withdraw(10)
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
-
-	assertError := func(t *testing.T, err error, want string) {
-		if err == nil {
-			t.Errorf("didn't get an error but wanted one")
-		}
-
-		if got := err.Error(); got != want {
-			t.Errorf("get %s, want %s", got, want)
-		}
-	}
 
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
 		startBalance := Bitcoin(20)
@@ -43,7 +62,7 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(100)
 
 		assertBalance(t, wallet, startBalance)
-		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertError(t, err, InsufficientFundsError)
 	})
 
 }
