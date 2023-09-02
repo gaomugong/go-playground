3cb0b0bf877395b02f13a9445adc8650eaabae7b
gaomugong <gaomugong@qq.com>
2023-09-02 15:39:12 +0800

    add find method with comma ok pattern

--- go-with-test/dicts/dictionary.go
+++ go-with-test/dicts/dictionary.go
@@ -1,7 +1,20 @@
 package dicts
 
+import (
+	"errors"
+)
+
 type Dict map[string]string
 
 func (d Dict) Search(s string) string {
 	return d[s]
+}
+
+var DictKeyError = errors.New("could not find the word you were looking for")
+
+func (d Dict) Find(s string) (string, error) {
+	if value, ok := d[s]; ok {
+		return value, nil
+	}
+	return "", DictKeyError
 }
--- go-with-test/dicts/dictionary_test.go
+++ go-with-test/dicts/dictionary_test.go
@@ -21,3 +21,31 @@ func assertString(t *testing.T, got string, want string) {
 		t.Errorf("got %#v, want %#v, given %#v", got, want, "test")
 	}
 }
+
+func assertError(t *testing.T, want, got error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %#v, want %#v", got, want)
+	}
+}
+
+func TestFind(t *testing.T) {
+	dict := Dict{"test": testStr}
+
+	t.Run("test: found", func(t *testing.T) {
+		got, _ := dict.Find("test")
+		want := testStr
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown: not found", func(t *testing.T) {
+		_, err := dict.Find("unknown")
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+		assertError(t, err, DictKeyError)
+	})
+
+}
