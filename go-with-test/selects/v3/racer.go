06a46245fd59c68aa3838555e84e23ce1e9d2e11
gaohongsong <ghs@qq.com>
2023-09-07 17:16:43 +0800

    feat: select new racer

--- go-with-test/selects/v3/racer.go
+++ go-with-test/selects/v3/racer.go
@@ -6,9 +6,15 @@ import (
 	"time"
 )
 
-// Racer select 中使用 ping 为两个 URL 设置两个 channel。
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a string, b string) (winner string, err error) {
+	return NewRacer(a, b, tenSecondTimeout)
+}
+
+// NewRacer select 中使用 ping 为两个 URL 设置两个 channel。
 // 无论哪个先写入其 channel 都会使 select 里的代码先被执行，这会导致那个 URL 先被返回
-func Racer(a string, b string, timeout time.Duration) (winner string, err error) {
+func NewRacer(a string, b string, timeout time.Duration) (winner string, err error) {
 	// select 则允许你同时在 多个 channel 等待。第一个发送值的 channel「胜出」，case 中的代码会被执行
 	select {
 	case <-ping(a):
--- go-with-test/selects/v3/racer_test.go
+++ go-with-test/selects/v3/racer_test.go
@@ -8,15 +8,29 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		slowServer := makeDelayedServer(time.Second * 12)
-		fastServer := makeDelayedServer(time.Second * 11)
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(time.Millisecond * 20)
+		fastServer := makeDelayedServer(time.Millisecond * 1)
 
 		// 把它放在你创建服务器语句附近，以便函数内后面的代码仍可以使用这个服务器
 		defer slowServer.Close()
 		defer fastServer.Close()
 
-		_, err := Racer(slowServer.URL, fastServer.URL, time.Second*10)
+		want := fastServer.URL
+		got, _ := Racer(slowServer.URL, fastServer.URL)
+		if want != got {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		aServer := makeDelayedServer(time.Millisecond * 25)
+
+		// 把它放在你创建服务器语句附近，以便函数内后面的代码仍可以使用这个服务器
+		defer aServer.Close()
+
+		// _, err := Racer(aServer.URL, aServer.URL)
+		_, err := NewRacer(aServer.URL, aServer.URL, 20*time.Millisecond)
 		if err == nil {
 			t.Errorf("expected an error but didn't get one")
 		}
