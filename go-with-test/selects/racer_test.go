857ffa41ef2615ef47d48393c8053d1daf5eaedb
gaohongsong <ghs@qq.com>
2023-09-07 14:55:57 +0800

    feat: new server

--- go-with-test/selects/racer_test.go
+++ go-with-test/selects/racer_test.go
@@ -1,13 +1,37 @@
 package selects
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	// v1
+	// slowURL := "http://www.facebook.com"
+	// fastURL := "http://www.quii.co.uk"
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+	// v2
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Millisecond * 20)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	// want := fastURL
+	want := fastServer.URL
+
+	// v1
+	// got := Racer(slowURL, fastURL)
+
+	// v2
+	fmt.Println(slowServer.URL, fastServer.URL)
+	got := Racer(slowServer.URL, fastServer.URL)
 
 	if want != got {
 		t.Errorf("Want %#v, got %#v", want, got)
