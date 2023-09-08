1ead8f1bbfed25fe5cde73573d86a249115df9ba
gaomugong <gaomugong@qq.com>
2023-09-08 18:09:31 +0800

    walk switch

--- go-with-test/reflects/walk.go
+++ go-with-test/reflects/walk.go
@@ -10,17 +10,31 @@ import (
 //	fn(field.String())
 //}
 
+//func walk(x interface{}, fn func(input string)) {
+//	val := reflect.ValueOf(x)
+//
+//	for i := 0; i < val.NumField(); i++ {
+//		field := val.Field(i)
+//
+//		if field.Kind() == reflect.String {
+//			fn(field.String())
+//		}
+//		if field.Kind() == reflect.Struct {
+//			//fmt.Println("find struct field")
+//			walk(field.Interface(), fn)
+//		}
+//	}
+//}
+
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
-		}
-		if field.Kind() == reflect.Struct {
-			//fmt.Println("find struct field")
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
