0ea7ed271caf754ad066170f8e3c40a4f9c16fc1
gaomugong <gaomugong@qq.com>
2023-09-10 14:58:01 +0800

    重构player

--- go-with-test/players/server/server_test.go
+++ go-with-test/players/server/server_test.go
@@ -1,39 +1,41 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+func assertResponse(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func newGetScoreRequest(player string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", player), nil)
+	return request
+}
+
 func TestPlayer(t *testing.T) {
 	t.Run("return Peter's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Petter", nil)
+		request := newGetScoreRequest("Petter")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertResponse(t, response.Body.String(), "20")
 
 	})
 
 	t.Run("return Floyd's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "10"
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
-
+		assertResponse(t, response.Body.String(), "10")
 	})
 }
