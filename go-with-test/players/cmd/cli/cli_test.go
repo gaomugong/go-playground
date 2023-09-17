2aee30e2fde30777d68ef60c16a769d0acd5a273
gaomugong <gaomugong@qq.com>
2023-09-17 18:10:32 +0800

    TestCLI

--- go-with-test/players/cmd/cli/cli_test.go
+++ go-with-test/players/cmd/cli/cli_test.go
@@ -1,24 +1,75 @@
 package main
 
 import (
+	"bufio"
+	"io"
 	. "server"
+	"strings"
 	"testing"
 )
 
 type CLI struct {
 	store PlayerStore
+	//in    io.Reader
+	in *bufio.Scanner
+}
+
+func NewCLI(store PlayerStore, in io.Reader) *CLI {
+	return &CLI{store: store, in: bufio.NewScanner(in)}
 }
 
 func (c *CLI) PlayPoker() {
-	c.store.RecordWin("Cleo")
+	//name := make([]byte, 100)
+	//c.in.Read(name)
+	//players := strings.Split(string(name), " ")
+	//fmt.Printf("players=%#v\n", players)
+	userInput := c.readLine()
+	c.store.RecordWin(extractWinner(userInput))
+}
+
+func extractWinner(input string) string {
+	return strings.Replace(input, " win", "", -1)
+}
+
+func (c *CLI) readLine() string {
+	c.in.Scan()
+	return c.in.Text()
+}
+
+func assertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.WinCalls()) != 1 {
+		t.Fatalf("got %d calls to RecordWin, want %d", len(store.WinCalls()), 1)
+	}
+
+	got := store.WinCalls()[0]
+	if got != winner {
+		t.Fatalf("did not store correct winner got %#v, want %#v", got, winner)
+	}
+
 }
 
 func TestCLI(t *testing.T) {
-	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
-	cli.PlayPoker()
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris win\n")
+		playerStore := &StubPlayerStore{}
 
-	if len(playerStore.WinCalls()) != 1 {
-		t.Fatalf("expected a win call but didn't get any")
-	}
+		//cli := &CLI{playerStore, in}
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo win\n")
+		playerStore := &StubPlayerStore{}
+
+		//cli := &CLI{playerStore, in}
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
 }
