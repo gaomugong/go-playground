0ca9b213bf6a6cc5b4e7c4cd75faa8ccb5723d81
gaomugong <gaomugong@qq.com>
2023-09-17 20:02:50 +0800

    TestCLI

--- go-with-test/players/cmd/cli/cli_test.go
+++ go-with-test/players/cmd/cli/cli_test.go
@@ -1,40 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"io"
 	. "server"
 	"strings"
 	"testing"
 )
-
-type CLI struct {
-	store PlayerStore
-	//in    io.Reader
-	in *bufio.Scanner
-}
-
-func NewCLI(store PlayerStore, in io.Reader) *CLI {
-	return &CLI{store: store, in: bufio.NewScanner(in)}
-}
-
-func (c *CLI) PlayPoker() {
-	//name := make([]byte, 100)
-	//c.in.Read(name)
-	//players := strings.Split(string(name), " ")
-	//fmt.Printf("players=%#v\n", players)
-	userInput := c.readLine()
-	c.store.RecordWin(extractWinner(userInput))
-}
-
-func extractWinner(input string) string {
-	return strings.Replace(input, " win", "", -1)
-}
-
-func (c *CLI) readLine() string {
-	c.in.Scan()
-	return c.in.Text()
-}
 
 func assertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
 	t.Helper()
--- go-with-test/players/cmd/cli/main.go
+++ go-with-test/players/cmd/cli/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	. "server"
+	"strings"
+)
+
+type CLI struct {
+	store PlayerStore
+	//in    io.Reader
+	in *bufio.Scanner
+}
+
+func NewCLI(store PlayerStore, in io.Reader) *CLI {
+	return &CLI{store: store, in: bufio.NewScanner(in)}
+}
+
+func (c *CLI) PlayPoker() {
+	//name := make([]byte, 100)
+	//c.in.Read(name)
+	//players := strings.Split(string(name), " ")
+	//fmt.Printf("players=%#v\n", players)
+	userInput := c.readLine()
+	c.store.RecordWin(extractWinner(userInput))
+}
+
+func extractWinner(input string) string {
+	return strings.Replace(input, " win", "", -1)
+}
+
+func (c *CLI) readLine() string {
+	c.in.Scan()
+	return c.in.Text()
+}
+
+const dbFileName = "game.db.json"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win}")
+
+	dbFile, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("failed to open database file %s: %v", dbFileName, err)
+	}
+
+	store, err := NewFileSystemStore(dbFile)
+	if err != nil {
+		log.Fatalf("failed to create store: %v", err)
+	}
+
+	game := NewCLI(store, os.Stdin)
+	game.PlayPoker()
 }
